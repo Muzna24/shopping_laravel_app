e7c5951de2c3a81648c36364d5d20d2c5ce3efba
Muzna24 <m.m.albarwania@gmail.com>
2022-06-26 18:10:02 +0400

    order table updated

--- database/migrations/2022_06_25_141312_create_orders_table.php
+++ database/migrations/2022_06_25_141312_create_orders_table.php
@@ -15,6 +15,7 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('status')->nullable();
             $table->unsignedInteger('admin_id');
             $table->unsignedInteger('customer_id');
             $table->timestamps();
