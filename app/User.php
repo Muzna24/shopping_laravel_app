48ed193c46cff3153199f589282445eb8086a927
Muzna24 <m.m.albarwania@gmail.com>
2022-06-27 12:26:16 +0400

    user & custmer relation changed

--- app/User.php
+++ app/User.php
@@ -37,8 +37,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function customers(){
-        return $this->hasMany(Customer::class);
+    public function customer(){
+        return $this->belongsTo(Customer::class);
     }
 
     public function admins(){
